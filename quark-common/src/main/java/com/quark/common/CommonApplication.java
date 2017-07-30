99558f79b9b6b521daa0e273396d4854e2aab97d
ChinaLHR <1420432344@qq.com>
2017-07-30 22:48:15 +0800

    init spring boot

---
+++ quark-common/src/main/java/com/quark/common/CommonApplication.java
@@ -0,0 +1,11 @@
+package com.quark.common;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by lhr on 17-7-30.
+ */
+@SpringBootApplication
+public class CommonApplication {
+
+}
---
+++ quark-common/src/test/java/com/quark/common/CommonApplicationTest.java
@@ -0,0 +1,14 @@
+package com.quark.common;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by lhr on 17-7-30.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommonApplicationTest {
+
+}
